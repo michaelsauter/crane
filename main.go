09f0237c4ec5446f855b553aaf1af804d1631852
Michael Sauter <mail@michaelsauter.net>
2014-01-03 11:51:09 +0000

    Use Cobra to define commands
    This commit replaces my implementation of subcommands with
    https://github.com/spf13/cobra.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
 )
@@ -9,66 +10,99 @@ import (
 var verbose bool
 
 func main() {
-	if len(os.Args) == 1 {
-		displayHelp()
-	} else {
-		// On panic, recover the error and display it
-		defer func() {
-			if err := recover(); err != nil {
-				fmt.Println("ERROR: ", err)
-			}
-		}()
-		// Set verbosity
-		setVerbosity()
-		// Read Cranefile
-		container := readCranefile("Cranefile")
-		// Run subcommand
-		switch os.Args[1] {
-		case "provision":
+	// On panic, recover the error and display it
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("ERROR: ", err)
+		}
+	}()
+
+	// Read the Cranefile in order to have
+	// a root container to execute the commands.
+	container := readCranefile("Cranefile")
+
+	var cmdProvision = &cobra.Command{
+		Use:   "provision",
+		Short: "Build or pull images",
+		Long: `
+provision will use specified Dockerfiles to build the images.
+If no Dockerfile is given, it will pull the image from the index.
+        `,
+		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Provisioning...")
 			container.provision()
-			break
+		},
+	}
 
-		case "run":
+	var cmdRun = &cobra.Command{
+		Use:   "run",
+		Short: "Run the containers",
+		Long:  `run will call docker run on all containers.`,
+		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Running...")
 			// "Entry" container is attachable
 			container.run(true)
-			break
+		},
+	}
 
-		case "rm":
+	var cmdRm = &cobra.Command{
+		Use:   "rm",
+		Short: "Remove the containers",
+		Long:  `rm will call docker rm on all containers.`,
+		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Removing...")
 			container.rm()
-			break
+		},
+	}
 
-		case "kill":
+	var cmdKill = &cobra.Command{
+		Use:   "kill",
+		Short: "Kill the containers",
+		Long:  `kill will call docker kill on all containers.`,
+		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Killing...")
 			container.kill()
-			break
+		},
+	}
 
-		case "start":
+	var cmdStart = &cobra.Command{
+		Use:   "start",
+		Short: "Start the containers",
+		Long:  `start will call docker start on all containers.`,
+		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Starting...")
 			container.start()
-			break
+		},
+	}
 
-		case "stop":
+	var cmdStop = &cobra.Command{
+		Use:   "stop",
+		Short: "Stop the containers",
+		Long:  `stop will call docker stop on all containers.`,
+		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Stopping...")
 			container.stop()
-			break
-
-		case "help":
-			displayHelp()
-			break
-
-		default:
-			fmt.Println("Command not found. See available commands with `crane help`.")
-			break
-		}
+		},
 	}
+
+	var craneCmd = &cobra.Command{
+		Use:   "crane",
+		Short: "crane - Lift containers with ease",
+		Long: `
+Crane is a little tool to orchestrate Docker containers.
+It works by reading in a Cranefile (a JSON file) which describes how to obtain container images and how to run them.
+See the corresponding docker commands for more information.
+		`,
+	}
+
+	craneCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	craneCmd.AddCommand(cmdProvision, cmdRun, cmdRm, cmdKill, cmdStart, cmdStop)
+	craneCmd.Execute()
 }
 
 func executeCommand(name string, args []string) {
 	if verbose {
-		fmt.Printf("%v\n", args)
+		fmt.Printf("Calling docker with %v\n", args)
 	}
 	cmd := exec.Command("docker", args...)
 	cmd.Stdout = os.Stdout
@@ -78,32 +112,4 @@ func executeCommand(name string, args []string) {
 	if !cmd.ProcessState.Success() {
 		panic(cmd.ProcessState.String()) // pass the error?
 	}
-}
-
-func setVerbosity() {
-	if len(os.Args) == 3 {
-		if os.Args[2] == "--verbose" {
-			verbose = true
-		}
-	}
-}
-
-func displayHelp() {
-	fmt.Println("crane - Lift containers with ease")
-	fmt.Println("")
-	fmt.Println("Usage:")
-	fmt.Println("")
-	fmt.Println("\tcrane command")
-	fmt.Println("")
-	fmt.Println("The commands are:")
-	fmt.Println("")
-	fmt.Println("\tprovision\tBuild or pull containers")
-	fmt.Println("\trun\t\tRun containers (linking them)")
-	fmt.Println("\tkill\t\tKill containers")
-	fmt.Println("\trm\t\tRemove containers")
-	fmt.Println("\tstart\t\tStart containers")
-	fmt.Println("\tstop\t\tStop containers")
-	fmt.Println("")
-	fmt.Println("See the docker commands for more information.")
-	fmt.Println("")
 }
