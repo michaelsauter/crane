591e7bac37c69d8d5332a456c76bd0fc301c7073
Michael Sauter <mail@michaelsauter.net>
2014-03-06 09:18:18 +1100

    Bump version to 0.5.1

--- main.go
+++ main.go
@@ -121,7 +121,7 @@ If no Dockerfile is given, it will pull the image from the index.
 		Short: "Display version",
 		Long:  `Displays the version of Crane.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("v0.5.0")
+			fmt.Println("v0.5.1")
 		},
 	}
 
