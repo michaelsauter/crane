cf2108e47657455926bcedb87f6b29306349cb37
Michael Sauter <mail@michaelsauter.net>
2014-03-21 13:29:26 +1100

    Move command handling into cmd.go

--- main.go
+++ main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/michaelsauter/crane/print"
-	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
 	"strings"
 )
-
-var verbose bool
-var force bool
-var kill bool
-var config string
 
 func main() {
 	// On panic, recover the error and display it
@@ -22,123 +16,11 @@ func main() {
 		}
 	}()
 
-	var cmdLift = &cobra.Command{
-		Use:   "lift",
-		Short: "Build or pull images, then run or start the containers",
-		Long: `
-provision will use specified Dockerfiles to build the images.
-If no Dockerfile is given, it will pull the image from the index.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			containers := getContainers(config)
-			containers.lift(force, kill)
-		},
-	}
-
-	var cmdProvision = &cobra.Command{
-		Use:   "provision",
-		Short: "Build or pull images",
-		Long: `
-provision will use specified Dockerfiles to build the images.
-If no Dockerfile is given, it will pull the image from the index.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			containers := getContainers(config)
-			containers.provision(force)
-		},
-	}
-
-	var cmdRun = &cobra.Command{
-		Use:   "run",
-		Short: "Run the containers",
-		Long:  `run will call docker run on all containers.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			containers := getContainers(config)
-			containers.run(force, kill)
-		},
-	}
-
-	var cmdRm = &cobra.Command{
-		Use:   "rm",
-		Short: "Remove the containers",
-		Long:  `rm will call docker rm on all containers.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			containers := getContainers(config)
-			containers.rm(force, kill)
-		},
-	}
-
-	var cmdKill = &cobra.Command{
-		Use:   "kill",
-		Short: "Kill the containers",
-		Long:  `kill will call docker kill on all containers.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			containers := getContainers(config)
-			containers.kill()
-		},
-	}
-
-	var cmdStart = &cobra.Command{
-		Use:   "start",
-		Short: "Start the containers",
-		Long:  `start will call docker start on all containers.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			containers := getContainers(config)
-			containers.start()
-		},
-	}
-
-	var cmdStop = &cobra.Command{
-		Use:   "stop",
-		Short: "Stop the containers",
-		Long:  `stop will call docker stop on all containers.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			containers := getContainers(config)
-			containers.stop()
-		},
-	}
-
-	var cmdStatus = &cobra.Command{
-		Use:   "status",
-		Short: "Displays status of containers",
-		Long:  `Displays the current status of the containers.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			containers := getContainers(config)
-			containers.status()
-		},
-	}
-
-	var cmdVersion = &cobra.Command{
-		Use:   "version",
-		Short: "Display version",
-		Long:  `Displays the version of Crane.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("v0.6.1")
-		},
-	}
-
-	var craneCmd = &cobra.Command{
-		Use:   "crane",
-		Short: "crane - Lift containers with ease",
-		Long: `
-Crane is a little tool to orchestrate Docker containers.
-It works by reading in JSON (either from a Cranefile or --config) which describes how to obtain container images and how to run them.
-See the corresponding docker commands for more information.`,
-	}
-
-	craneCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
-	craneCmd.PersistentFlags().StringVarP(&config, "config", "c", "", "config to read from")
-	cmdLift.Flags().BoolVarP(&force, "force", "f", false, "force")
-	cmdLift.Flags().BoolVarP(&kill, "kill", "k", false, "kill containers")
-	cmdProvision.Flags().BoolVarP(&force, "force", "f", false, "force")
-	cmdRun.Flags().BoolVarP(&force, "force", "f", false, "force")
-	cmdRun.Flags().BoolVarP(&kill, "kill", "k", false, "kill containers")
-	cmdRm.Flags().BoolVarP(&force, "force", "f", false, "force")
-	cmdRm.Flags().BoolVarP(&kill, "kill", "k", false, "kill containers")
-	craneCmd.AddCommand(cmdLift, cmdProvision, cmdRun, cmdRm, cmdKill, cmdStart, cmdStop, cmdStatus, cmdVersion)
-	craneCmd.Execute()
+	handleCmd()
 }
 
 func executeCommand(name string, args []string) {
-	if verbose {
+	if isVerbose() {
 		fmt.Printf("\n--> %s %s\n", name, strings.Join(args, " "))
 	}
 	cmd := exec.Command(name, args...)
