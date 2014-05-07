9db56634ae5ef5d46512233a6d506a754a3b2b3b
Michael Sauter <mail@michaelsauter.net>
2014-05-07 23:47:56 +0000

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,21 @@
+version=$1
+
+if [ -z "$version" ]; then
+  echo "No version passed! Example usage: ./release.sh 1.0.0"
+  exit 1
+fi
+
+echo "Update version command..."
+sed -i.bak 's/fmt\.Println("v[0-9]\.[0-9]\.[0-9]")/fmt.Println("v'$version'")/' cmd.go
+rm cmd.go.bak
+
+echo "Build binary..."
+../../../../bin/gox -osarch="darwin/amd64" -osarch="linux/amd64" 
+
+echo "Update repository..."
+git add cmd.go 
+git commit -m "Bump version to ${version}"
+git tag "v$version"
+
+echo "v$version tagged."
+echo "Now, run 'git push origin master && git push --tags' and publish the release on GitHub."
