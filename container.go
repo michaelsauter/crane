685d2ceeed40c7de6ff69c6e222f82aa6ae731e2
Michael Sauter <mail@michaelsauter.net>
2014-05-17 22:33:11 +0000

    Expand environment variables for all run parameters
    Closes #33.

--- container.go
+++ container.go
@@ -19,29 +19,29 @@ type Container struct {
 }
 
 type RunParameters struct {
-	Cidfile     string      `json:"cidfile" yaml:"cidfile"`
-	CpuShares   int         `json:"cpu-shares" yaml:"cpu-shares"`
-	Detach      bool        `json:"detach" yaml:"detach"`
-	Dns         []string    `json:"dns" yaml:"dns"`
-	Entrypoint  string      `json:"entrypoint" yaml:"entrypoint"`
-	Env         []string    `json:"env" yaml:"env"`
-	EnvFile     string      `json:"env-file" yaml:"env-file"`
-	Expose      []string    `json:"expose" yaml:"expose"`
-	Hostname    string      `json:"hostname" yaml:"hostname"`
-	Interactive bool        `json:"interactive" yaml:"interactive"`
-	Link        []string    `json:"link" yaml:"link"`
-	LxcConf     []string    `json:"lxc-conf" yaml:"lxc-conf"`
-	Memory      string      `json:"memory" yaml:"memory"`
-	Privileged  bool        `json:"privileged" yaml:"privileged"`
-	Publish     []string    `json:"publish" yaml:"publish"`
-	PublishAll  bool        `json:"publish-all" yaml:"publish-all"`
-	Rm          bool        `json:"rm" yaml:"rm"`
-	Tty         bool        `json:"tty" yaml:"tty"`
-	User        string      `json:"user" yaml:"user"`
-	Volume      []string    `json:"volume" yaml:"volume"`
-	VolumesFrom []string    `json:"volumes-from" yaml:"volumes-from"`
-	Workdir     string      `json:"workdir" yaml:"workdir"`
-	Command     interface{} `json:"cmd" yaml:"cmd"`
+	RawCidfile     string      `json:"cidfile" yaml:"cidfile"`
+	CpuShares      int         `json:"cpu-shares" yaml:"cpu-shares"`
+	Detach         bool        `json:"detach" yaml:"detach"`
+	RawDns         []string    `json:"dns" yaml:"dns"`
+	RawEntrypoint  string      `json:"entrypoint" yaml:"entrypoint"`
+	RawEnv         []string    `json:"env" yaml:"env"`
+	RawEnvFile     string      `json:"env-file" yaml:"env-file"`
+	RawExpose      []string    `json:"expose" yaml:"expose"`
+	RawHostname    string      `json:"hostname" yaml:"hostname"`
+	Interactive    bool        `json:"interactive" yaml:"interactive"`
+	RawLink        []string    `json:"link" yaml:"link"`
+	RawLxcConf     []string    `json:"lxc-conf" yaml:"lxc-conf"`
+	RawMemory      string      `json:"memory" yaml:"memory"`
+	Privileged     bool        `json:"privileged" yaml:"privileged"`
+	RawPublish     []string    `json:"publish" yaml:"publish"`
+	PublishAll     bool        `json:"publish-all" yaml:"publish-all"`
+	Rm             bool        `json:"rm" yaml:"rm"`
+	Tty            bool        `json:"tty" yaml:"tty"`
+	RawUser        string      `json:"user" yaml:"user"`
+	RawVolume      []string    `json:"volume" yaml:"volume"`
+	RawVolumesFrom []string    `json:"volumes-from" yaml:"volumes-from"`
+	RawWorkdir     string      `json:"workdir" yaml:"workdir"`
+	RawCmd         interface{} `json:"cmd" yaml:"cmd"`
 }
 
 func (container *Container) Name() string {
@@ -54,6 +54,124 @@ func (container *Container) Dockerfile() string {
 
 func (container *Container) Image() string {
 	return os.ExpandEnv(container.RawImage)
+}
+
+func (r *RunParameters) Cidfile() string {
+	return os.ExpandEnv(r.RawCidfile)
+}
+
+func (r *RunParameters) Dns() []string {
+	var dns []string
+	for _, rawDns := range r.RawDns {
+		dns = append(dns, os.ExpandEnv(rawDns))
+	}
+	return dns
+}
+
+func (r *RunParameters) Entrypoint() string {
+	return os.ExpandEnv(r.RawEntrypoint)
+}
+
+func (r *RunParameters) Env() []string {
+	var env []string
+	for _, rawEnv := range r.RawEnv {
+		env = append(env, os.ExpandEnv(rawEnv))
+	}
+	return env
+}
+
+func (r *RunParameters) EnvFile() string {
+	return os.ExpandEnv(r.RawEnvFile)
+}
+
+func (r *RunParameters) Expose() []string {
+	var expose []string
+	for _, rawExpose := range r.RawExpose {
+		expose = append(expose, os.ExpandEnv(rawExpose))
+	}
+	return expose
+}
+
+func (r *RunParameters) Hostname() string {
+	return os.ExpandEnv(r.RawHostname)
+}
+
+func (r *RunParameters) Link() []string {
+	var link []string
+	for _, rawLink := range r.RawLink {
+		link = append(link, os.ExpandEnv(rawLink))
+	}
+	return link
+}
+
+func (r *RunParameters) LxcConf() []string {
+	var lxcConf []string
+	for _, rawLxcConf := range r.RawLxcConf {
+		lxcConf = append(lxcConf, os.ExpandEnv(rawLxcConf))
+	}
+	return lxcConf
+}
+
+func (r *RunParameters) Memory() string {
+	return os.ExpandEnv(r.RawMemory)
+}
+
+func (r *RunParameters) Publish() []string {
+	var publish []string
+	for _, rawPublish := range r.RawPublish {
+		publish = append(publish, os.ExpandEnv(rawPublish))
+	}
+	return publish
+}
+
+func (r *RunParameters) User() string {
+	return os.ExpandEnv(r.RawUser)
+}
+
+func (r *RunParameters) Volume() []string {
+	var volume []string
+	for _, rawVolume := range r.RawVolume {
+		paths := strings.Split(rawVolume, ":")
+		if !path.IsAbs(paths[0]) {
+			cwd, _ := os.Getwd()
+			paths[0] = cwd + "/" + paths[0]
+		}
+		volume = append(volume, os.ExpandEnv(strings.Join(paths, ":")))
+	}
+	return volume
+}
+
+func (r *RunParameters) VolumesFrom() []string {
+	var volumesFrom []string
+	for _, rawVolumesFrom := range r.RawVolumesFrom {
+		volumesFrom = append(volumesFrom, os.ExpandEnv(rawVolumesFrom))
+	}
+	return volumesFrom
+}
+
+func (r *RunParameters) Workdir() string {
+	return os.ExpandEnv(r.RawWorkdir)
+}
+
+func (r *RunParameters) Cmd() []string {
+	var cmd []string
+	if r.RawCmd != nil {
+		switch rawCmd := r.RawCmd.(type) {
+		case string:
+			if len(rawCmd) > 0 {
+				cmd = append(cmd, os.ExpandEnv(rawCmd))
+			}
+		case []interface{}:
+			cmds := make([]string, len(rawCmd))
+			for i, v := range rawCmd {
+				cmds[i] = os.ExpandEnv(v.(string))
+			}
+			cmd = append(cmd, cmds...)
+		default:
+			print.Error("cmd is of unknown type!")
+		}
+	}
+	return cmd
 }
 
 func (container *Container) Id() (id string, err error) {
@@ -186,64 +304,64 @@ func (container Container) run() {
 		// Assemble command arguments
 		args := []string{"run"}
 		// Cidfile
-		if len(container.Run.Cidfile) > 0 {
-			args = append(args, "--cidfile", os.ExpandEnv(container.Run.Cidfile))
+		if len(container.Run.Cidfile()) > 0 {
+			args = append(args, "--cidfile", container.Run.Cidfile())
 		}
 		// CPU shares
 		if container.Run.CpuShares > 0 {
-			args = append(args, "--cpu-shares", os.ExpandEnv(strconv.Itoa(container.Run.CpuShares)))
+			args = append(args, "--cpu-shares", strconv.Itoa(container.Run.CpuShares))
 		}
 		// Detach
 		if container.Run.Detach {
 			args = append(args, "--detach")
 		}
 		// Dns
-		for _, dns := range container.Run.Dns {
-			args = append(args, "--dns", os.ExpandEnv(dns))
+		for _, dns := range container.Run.Dns() {
+			args = append(args, "--dns", dns)
 		}
 		// Entrypoint
-		if len(container.Run.Entrypoint) > 0 {
-			args = append(args, "--entrypoint", os.ExpandEnv(container.Run.Entrypoint))
+		if len(container.Run.Entrypoint()) > 0 {
+			args = append(args, "--entrypoint", container.Run.Entrypoint())
 		}
 		// Env
-		for _, env := range container.Run.Env {
-			args = append(args, "--env", os.ExpandEnv(env))
+		for _, env := range container.Run.Env() {
+			args = append(args, "--env", env)
 		}
 		// Env file
-		if len(container.Run.EnvFile) > 0 {
-			args = append(args, "--env-file", os.ExpandEnv(container.Run.EnvFile))
+		if len(container.Run.EnvFile()) > 0 {
+			args = append(args, "--env-file", container.Run.EnvFile())
 		}
 		// Expose
-		for _, expose := range container.Run.Expose {
-			args = append(args, "--expose", os.ExpandEnv(expose))
+		for _, expose := range container.Run.Expose() {
+			args = append(args, "--expose", expose)
 		}
 		// Host
-		if len(container.Run.Hostname) > 0 {
-			args = append(args, "--hostname", os.ExpandEnv(container.Run.Hostname))
+		if len(container.Run.Hostname()) > 0 {
+			args = append(args, "--hostname", container.Run.Hostname())
 		}
 		// Interactive
 		if container.Run.Interactive {
 			args = append(args, "--interactive")
 		}
 		// Link
-		for _, link := range container.Run.Link {
+		for _, link := range container.Run.Link() {
 			args = append(args, "--link", link)
 		}
 		// LxcConf
-		for _, lxcConf := range container.Run.LxcConf {
-			args = append(args, "--lxc-conf", os.ExpandEnv(lxcConf))
+		for _, lxcConf := range container.Run.LxcConf() {
+			args = append(args, "--lxc-conf", lxcConf)
 		}
 		// Memory
-		if len(container.Run.Memory) > 0 {
-			args = append(args, "--memory", os.ExpandEnv(container.Run.Memory))
+		if len(container.Run.Memory()) > 0 {
+			args = append(args, "--memory", container.Run.Memory())
 		}
 		// Privileged
 		if container.Run.Privileged {
 			args = append(args, "--privileged")
 		}
 		// Publish
-		for _, port := range container.Run.Publish {
-			args = append(args, "--publish", os.ExpandEnv(port))
+		for _, port := range container.Run.Publish() {
+			args = append(args, "--publish", port)
 		}
 		// PublishAll
 		if container.Run.PublishAll {
@@ -258,48 +376,27 @@ func (container Container) run() {
 			args = append(args, "--tty")
 		}
 		// User
-		if len(container.Run.User) > 0 {
-			args = append(args, "--user", os.ExpandEnv(container.Run.User))
+		if len(container.Run.User()) > 0 {
+			args = append(args, "--user", container.Run.User())
 		}
 		// Volumes
-		for _, volume := range container.Run.Volume {
-			paths := strings.Split(volume, ":")
-			if !path.IsAbs(paths[0]) {
-				cwd, _ := os.Getwd()
-				paths[0] = cwd + "/" + paths[0]
-			}
-			args = append(args, "--volume", os.ExpandEnv(strings.Join(paths, ":")))
+		for _, volume := range container.Run.Volume() {
+			args = append(args, "--volume", volume)
 		}
 		// VolumesFrom
-		for _, volumeFrom := range container.Run.VolumesFrom {
-			args = append(args, "--volumes-from", os.ExpandEnv(volumeFrom))
+		for _, volumeFrom := range container.Run.VolumesFrom() {
+			args = append(args, "--volumes-from", volumeFrom)
 		}
 		// Workdir
-		if len(container.Run.Workdir) > 0 {
-			args = append(args, "--workdir", os.ExpandEnv(container.Run.Workdir))
+		if len(container.Run.Workdir()) > 0 {
+			args = append(args, "--workdir", container.Run.Workdir())
 		}
-
 		// Name
 		args = append(args, "--name", container.Name())
 		// Image
 		args = append(args, container.Image())
 		// Command
-		if container.Run.Command != nil {
-			switch cmd := container.Run.Command.(type) {
-			case string:
-				if len(cmd) > 0 {
-					args = append(args, cmd)
-				}
-			case []interface{}:
-				cmds := make([]string, len(cmd))
-				for i, v := range cmd {
-					cmds[i] = v.(string)
-				}
-				args = append(args, cmds...)
-			default:
-				print.Error("cmd is of unknown type!")
-			}
-		}
+		args = append(args, container.Run.Cmd()...)
 		// Execute command
 		executeCommand("docker", args)
 	}
