1f8cbc5d364e6896c3754771cdf482e4ecc4ec50
Brice Jaglin <bjaglin@teads.tv>
2014-07-07 14:56:45 +0200

    ensure env variables are expanded in dependencies

--- crane/container_test.go
+++ crane/container_test.go
@@ -7,9 +7,13 @@ func TestDependencies(t *testing.T) {
 	if deps := container.Dependencies(); deps[0] != "a" || deps[1] != "b" || deps[2] != "c" {
 		t.Errorf("Dependencies should have been a, b and c. Got %v", deps)
 	}
+	container = &Container{Run: RunParameters{RawLink: []string{"${DOES_NOT_EXIST}a:b", "${DOES_NOT_EXIST_EITHER}b:d"}}}
+	if deps := container.Dependencies(); deps[0] != "a" || deps[1] != "b" {
+		t.Errorf("Dependencies should have been [a b]. Got %v", deps)
+	}
 	container = &Container{Run: RunParameters{RawLink: []string{}, RawVolumesFrom: []string{}}}
 	if deps := container.Dependencies(); len(deps) != 0 {
-		t.Error("Dependencies should have been empty")
+		t.Error("Dependencies should have been empty. Got %v", deps)
 	}
 }
 
