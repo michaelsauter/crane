e85de193018e00795c54eb30730c82172d5a2ce9
Michael Sauter <mail@michaelsauter.net>
2014-07-12 08:58:57 +0000

    Add documentation

--- crane/container_map.go
+++ crane/container_map.go
@@ -5,15 +5,26 @@ import (
 	"strings"
 )
 
+// ContainerMap maps the container name
+// to its configuration
 type ContainerMap map[string]Container
 
+// Unordered maps a container name
+// to its dependencies
 type Unordered map[string]*Dependencies
 
+// Dependencies contain two fields:
+// list: contains all dependencies
+// linked: contains dependencies that
+// are being linked to (which means they
+// need to be running in order to be satisfied).
 type Dependencies struct {
 	list   []string
 	linked []string
 }
 
+// includes checks whether the given needle is
+// included in the dependency list
 func (d *Dependencies) includes(needle string) bool {
 	for _, name := range d.list {
 		if name == needle {
@@ -23,6 +34,9 @@ func (d *Dependencies) includes(needle string) bool {
 	return false
 }
 
+// mustRun checks whether the given needle needs
+// to be running (for now that just means it's in
+// the linked list)
 func (d *Dependencies) mustRun(needle string) bool {
 	for _, name := range d.linked {
 		if name == needle {
@@ -32,10 +46,12 @@ func (d *Dependencies) mustRun(needle string) bool {
 	return false
 }
 
+// satisfied is true when the list is empty
 func (d *Dependencies) satisfied() bool {
 	return len(d.list) == 0
 }
 
+// remove removes the given name from the list
 func (d *Dependencies) remove(resolved string) {
 	for i, name := range d.list {
 		if name == resolved {
@@ -44,6 +60,15 @@ func (d *Dependencies) remove(resolved string) {
 	}
 }
 
+// order works on the containerMap and retuens the order
+// The order can be reversed as well. This is needed as any command
+// bringing up containers needs to bring up the dependencies first,
+// but any command shutting down containers needs to shutdown the
+// dependent containers first.
+// If the order between 2 containers does not matter, they are sorted
+// alphabetically.
+// If the map cannot be resolved, and error is returned detailing
+// which containers still have unresolved dependencies.
 func (m ContainerMap) order(reversed bool) (order []string, err error) {
 	unordered := m.unordered(reversed)
 	alphabetical := m.alphabetical(reversed)
@@ -118,6 +143,9 @@ func (m ContainerMap) order(reversed bool) (order []string, err error) {
 	return
 }
 
+// unordered returns a map describing the dependencies
+// between the containers. This is used as a basis to
+// determine the order.
 func (m ContainerMap) unordered(reversed bool) Unordered {
 	unordered := make(map[string]*Dependencies)
 
@@ -152,6 +180,9 @@ func (m ContainerMap) unordered(reversed bool) Unordered {
 	return unordered
 }
 
+// alphabetical returns the containers of the map in
+// alphabetical order. If reversed is set to true, it
+// returns reverse alphabetical order.
 func (m ContainerMap) alphabetical(reversed bool) []string {
 	alphabetical := []string{}
 	inserted := false
@@ -180,6 +211,9 @@ func (m ContainerMap) alphabetical(reversed bool) []string {
 	return alphabetical
 }
 
+// resolve deletes the given name from the
+// unordered map and remoes it from all
+// dependencies.
 func (u Unordered) resolve(resolved string) {
 	if _, ok := u[resolved]; ok {
 		delete(u, resolved)
