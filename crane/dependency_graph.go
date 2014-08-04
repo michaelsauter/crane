michaelsauter/crane

commit 71b9c0a35b063c883abd2d1ff3fad97a01165eca

Refactor dependency resolution
The biggest change is the removal of reversed order from the
dependency resolution. This makes the logic quite a bit easier
to understand.
The order is only reversed right before the containers are used.
This change would produce sub-optimal reverse orders for targets
with missing dependencies, so additionally it introduces the
notion of a forced order, yielding the same results as before.
At the same time, this  allows the user to execute targets that
can't be satisfied but don't need to (status, provision and push).

crane/dependency_graph.go
@@ -1,0 +1,100 @@
+package crane
+
+import (
+	"fmt"
+	"strings"
+)
+
+// DependencyGraph maps container names
+// to their dependencies
+type DependencyGraph map[string]*Dependencies
+
+// determineOrder works on the dependency graph and returns the order
+// of the given the target (a subset of the graph).
+// If force is true, the map will be ordered even if dependencies
+// are missing.
+// If force is false and the graph cannot be resolved properly,
+// an error is returned.
+func (graph DependencyGraph) order(target Target, force bool) (order []string, err error) {
+	success := true
+	for success && len(order) < len(target) {
+		success = false
+
+		// Try to resolve container with satisfied dependencies
+		for _, name := range target {
+			dependencies := graph[name]
+			if dependencies.satisfied() {
+				// Resolve "name" and continue with next iteration
+				success = true
+				order = append([]string{name}, order...)
+				graph.resolve(name)
+				break
+			}
+		}
+
+		// If we could not resolve a container so far in this iteration,
+		// we check if one of the non-targeted dependencies already runs or exists.
+		if !success {
+			for _, name := range target {
+				dependencies := graph[name]
+				for _, name := range dependencies.list {
+					if !target.includes(name) {
+						container := &Container{RawName: name}
+						satisfied := false
+						if dependencies.mustRun(name) {
+							satisfied = container.running()
+						} else {
+							satisfied = container.exists()
+						}
+						if satisfied {
+							success = true
+							graph.resolve(name)
+							break
+						}
+					}
+				}
+			}
+		}
+
+		// If we still have not resolved a dependency, we can't
+		// order the given target properly. However, if the order
+		// is forced, we just resolve the first non-targeted
+		// dependency.
+		if !success && force {
+			for _, name := range target {
+				dependencies := graph[name]
+				for _, name := range dependencies.list {
+					if !target.includes(name) {
+						success = true
+						graph.resolve(name)
+						break
+					}
+				}
+			}
+		}
+	}
+
+	// If we the order is not complete yet, the target
+	// cannot be resolved (cyclic or missing dependency found).
+	if len(order) < len(target) {
+		unresolved := []string{}
+		for _, name := range target {
+			if _, ok := graph[name]; ok {
+				unresolved = append(unresolved, name)
+			}
+		}
+		err = fmt.Errorf("Dependencies for container(s) %s could not be resolved. Check for cyclic or missing dependencies.", strings.Join(unresolved, ", "))
+	}
+
+	return
+}
+
+// resolve deletes the given name from the
+// dependency graph and removes it from all
+// dependencies.
+func (d DependencyGraph) resolve(resolved string) {
+	delete(d, resolved)
+	for _, dependencies := range d {
+		dependencies.remove(resolved)
+	}
+}

crane/dependency_graph_test.go
@@ -1,0 +1,64 @@
+package crane
+
+import "testing"
+
+func TestOrder(t *testing.T) {
+	var err error
+	var order []string
+	var dependencyGraph DependencyGraph
+
+	// Resolvable map
+	dependencyGraph = DependencyGraph{
+		"b": &Dependencies{list: []string{"c"}},
+		"a": &Dependencies{list: []string{"b"}},
+		"c": &Dependencies{list: []string{}},
+	}
+	order, err = dependencyGraph.order([]string{"a", "b", "c"}, false)
+	if err != nil || len(order) != 3 || order[0] != "a" || order[1] != "b" || order[2] != "c" {
+		t.Errorf("Order should have been [a b c], got %v. Err: %v", order, err)
+	}
+
+	// Cyclic map without forced order fails
+	dependencyGraph = DependencyGraph{
+		"b": &Dependencies{list: []string{"c"}},
+		"a": &Dependencies{list: []string{"b"}},
+		"c": &Dependencies{list: []string{"a"}},
+	}
+	order, err = dependencyGraph.order([]string{"a", "b", "c"}, false)
+	if err == nil {
+		t.Errorf("Cyclic dependency a -> b -> c -> a should not have been resolvable, got %v. Err: %v", order, err)
+	}
+
+	// Cyclic map with forced order fails
+	dependencyGraph = DependencyGraph{
+		"b": &Dependencies{list: []string{"c"}},
+		"a": &Dependencies{list: []string{"b"}},
+		"c": &Dependencies{list: []string{"a"}},
+	}
+	order, err = dependencyGraph.order([]string{"a", "b", "c"}, true)
+	if err == nil {
+		t.Errorf("Cyclic dependency a -> b -> c -> a should not have been resolvable, got %v. Err: %v", order, err)
+	}
+
+	// Resolvable map, partial target fails non-forced
+	dependencyGraph = DependencyGraph{
+		"b": &Dependencies{list: []string{"c"}},
+		"a": &Dependencies{list: []string{"b"}},
+		"c": &Dependencies{list: []string{}},
+	}
+	order, err = dependencyGraph.order([]string{"a", "b"}, false)
+	if err == nil {
+		t.Errorf("Dependency c was missing, so graph should not have been resolvable, got %v. Err: %v", order, err)
+	}
+
+	// Resolvable map, partial target, succeeds forced
+	dependencyGraph = DependencyGraph{
+		"b": &Dependencies{list: []string{"c"}},
+		"a": &Dependencies{list: []string{"b"}},
+		"c": &Dependencies{list: []string{}},
+	}
+	order, err = dependencyGraph.order([]string{"a", "b"}, true)
+	if err != nil || len(order) != 2 || order[0] != "a" || order[1] != "b" {
+		t.Errorf("Dependency c was missing, but order was forced, so graph should have been resolvable, got %v. Err: %v", order, err)
+	}
+}

crane/cmd.go
@@ -30,14 +30,14 @@ func isVerbose() bool {
 }
 
 // returns a function to be set as a cobra command run, wrapping a command meant to be run on a set of containers
-func containersCommand(wrapped func(containers Containers), reversed bool) func(cmd *cobra.Command, args []string) {
+func containersCommand(wrapped func(containers Containers), forceOrder bool) func(cmd *cobra.Command, args []string) {
 	return func(cmd *cobra.Command, args []string) {
 		if len(args) > 0 {
 			cmd.Printf("Error: too many arguments given: %#q", args)
 			cmd.Usage()
 			panic(StatusError{status: 64})
 		}
-		wrapped(NewConfig(options, reversed).Containers())
+		wrapped(NewConfig(options, forceOrder).Containers())
 	}
 }
 
@@ -61,7 +61,7 @@ provision will use specified Dockerfiles to build all targeted images.
 If no Dockerfile is given, it will pull the image(s) from the given registry.`,
 		Run: containersCommand(func(containers Containers) {
 			containers.provision(options.nocache)
-		}, false),
+		}, true),
 	}
 
 	var cmdRun = &cobra.Command{
@@ -78,7 +78,7 @@ If no Dockerfile is given, it will pull the image(s) from the given registry.`,
 		Short: "Remove the containers",
 		Long:  `rm will call docker rm for all targeted containers.`,
 		Run: containersCommand(func(containers Containers) {
-			containers.rm(options.kill)
+			containers.reversed().rm(options.kill)
 		}, true),
 	}
 
@@ -87,7 +87,7 @@ If no Dockerfile is given, it will pull the image(s) from the given registry.`,
 		Short: "Kill the containers",
 		Long:  `kill will call docker kill for all targeted containers.`,
 		Run: containersCommand(func(containers Containers) {
-			containers.kill()
+			containers.reversed().kill()
 		}, true),
 	}
 
@@ -105,7 +105,7 @@ If no Dockerfile is given, it will pull the image(s) from the given registry.`,
 		Short: "Stop the containers",
 		Long:  `stop will call docker stop for all targeted containers.`,
 		Run: containersCommand(func(containers Containers) {
-			containers.stop()
+			containers.reversed().stop()
 		}, true),
 	}
 
@@ -114,7 +114,7 @@ If no Dockerfile is given, it will pull the image(s) from the given registry.`,
 		Short: "Pause the containers",
 		Long:  `pause will call docker pause for all targeted containers.`,
 		Run: containersCommand(func(containers Containers) {
-			containers.pause()
+			containers.reversed().pause()
 		}, true),
 	}
 
@@ -133,7 +133,7 @@ If no Dockerfile is given, it will pull the image(s) from the given registry.`,
 		Long:  `push will call docker push for all targeted containers.`,
 		Run: containersCommand(func(containers Containers) {
 			containers.push()
-		}, false),
+		}, true),
 	}
 
 	var cmdStatus = &cobra.Command{
@@ -142,7 +142,7 @@ If no Dockerfile is given, it will pull the image(s) from the given registry.`,
 		Long:  `Displays the current status of all targeted containers.`,
 		Run: containersCommand(func(containers Containers) {
 			containers.status(options.notrunc)
-		}, false),
+		}, true),
 	}
 
 	var cmdVersion = &cobra.Command{

crane/config.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 )
 
 type Config struct {
@@ -16,9 +17,17 @@ type Config struct {
 	RawOrder        []string            `json:"order" yaml:"order"`
 	RawGroups       map[string][]string `json:"groups" yaml:"groups"`
 	containerMap    ContainerMap
+	dependencyGraph DependencyGraph
+	target          Target
 	order           []string
 	groups          map[string][]string
 }
+
+// ContainerMap maps the container name
+// to its configuration
+type ContainerMap map[string]Container
+
+type Target []string
 
 // configFiles returns a slice of
 // files to read the config from.
@@ -98,7 +107,7 @@ func unmarshalYAML(data []byte) *Config {
 	return config
 }
 
-func NewConfig(options Options, reversed bool) *Config {
+func NewConfig(options Options, forceOrder bool) *Config {
 	var config *Config
 	for _, f := range configFiles(options) {
 		if _, err := os.Stat(f); err == nil {
@@ -109,9 +118,10 @@ func NewConfig(options Options, reversed bool) *Config {
 	if config == nil {
 		panic(StatusError{fmt.Errorf("No configuration found %v", configFiles(options)), 78})
 	}
-	config.process()
-	config.truncate(options.target)
-	err := config.determineOrder(reversed)
+	config.expandEnv()
+	config.determineGraph()
+	config.determineTarget(options.target)
+	err := config.determineOrder(forceOrder)
 	if err != nil {
 		panic(StatusError{err, 78})
 	}
@@ -127,11 +137,11 @@ func (c *Config) Containers() Containers {
 	return containers
 }
 
-// process creates a new container map
+// expandEnv creates a new container map
 // with expanded names and sets the RawName of each
 // container to the map key.
 // It also expand variables in the order and the groups.
-func (c *Config) process() {
+func (c *Config) expandEnv() {
 	// Container map
 	c.containerMap = make(map[string]Container)
 	for rawName, container := range c.RawContainerMap {
@@ -152,15 +162,24 @@ func (c *Config) process() {
 	}
 }
 
+// generateGraph generated the dependency graph, which is
+// a map describing the dependencies between the containers.
+func (c *Config) determineGraph() {
+	c.dependencyGraph = make(DependencyGraph)
+	for _, container := range c.containerMap {
+		c.dependencyGraph[container.Name()] = container.Dependencies()
+	}
+}
+
 // determineOrder sets the Order field of the config.
 // Containers will be ordered so that they can be
 // brought up and down with Docker.
-func (c *Config) determineOrder(reversed bool) error {
+func (c *Config) determineOrder(force bool) error {
 	if len(c.order) > 0 {
 		return nil // Order was set manually
 	}
 
-	order, err := c.containerMap.order(reversed)
+	order, err := c.dependencyGraph.order(c.target, force)
 	if err != nil {
 		return err
 	} else {
@@ -169,16 +188,20 @@ func (c *Config) determineOrder(reversed bool) error {
 	return nil
 }
 
-// truncate receives a target and deletes all containers
-// from the map which are not targeted.
-func (c *Config) truncate(target string) {
-	included := c.targetedContainers(target)
-	c.containerMap = c.containerMap.subset(included)
+// determineTarget receives the specified target
+// and determines which containers should be targeted.
+// Additionally, ot sorts these alphabetically.
+func (c *Config) determineTarget(target string) {
+	c.target = Target(c.explicitlyTargeted(target))
+
+	// Extend target here (cascade)
+
+	sort.Strings(c.target)
 }
 
-// targetedContainers receives a target and determines which
+// explicitlyTargeted receives a target and determines which
 // containers of the map are targeted
-func (c *Config) targetedContainers(target string) []string {
+func (c *Config) explicitlyTargeted(target string) []string {
 	// target not given
 	if len(target) == 0 {
 		// If default group exists, return its containers
@@ -210,4 +233,15 @@ func (c *Config) targetedContainers(target string) []string {
 	}
 	// Otherwise, fail verbosely
 	panic(StatusError{fmt.Errorf("No group or container matching `%s`", target), 64})
+}
+
+// includes checks whether the given needle is
+// included in the dependency list
+func (t Target) includes(needle string) bool {
+	for _, name := range t {
+		if name == needle {
+			return true
+		}
+	}
+	return false
 }

crane/config_test.go
@@ -17,13 +17,13 @@ func TestConfigFiles(t *testing.T) {
 	}
 }
 
-func TestSetNames(t *testing.T) {
+func TestExpandEnv(t *testing.T) {
 	rawContainerMap := ContainerMap{
 		"a": Container{},
 		"b": Container{},
 	}
 	c := &Config{RawContainerMap: rawContainerMap}
-	c.process()
+	c.expandEnv()
 	if c.containerMap["a"].RawName != "a" || c.containerMap["b"].RawName != "b" {
 		t.Errorf("Names should be 'a' and 'b', got %s and %s", c.containerMap["a"].RawName, c.containerMap["b"].RawName)
 	}
@@ -33,14 +33,14 @@ func TestDetermineOrder(t *testing.T) {
 	// Order set manually
 	rawOrder := []string{"a", "b", "c"}
 	c := &Config{RawOrder: rawOrder}
-	c.process()
+	c.expandEnv()
 	c.determineOrder(false)
 	if c.order[0] != "a" || c.order[1] != "b" || c.order[2] != "c" {
 		t.Errorf("Order should have been %v, got %v", rawOrder, c.order)
 	}
 }
 
-func TestTargetedContainers(t *testing.T) {
+func TestExplicitlyTargeted(t *testing.T) {
 	var result []string
 	var containers []string
 	var rawGroups = make(map[string][]string)
@@ -57,16 +57,16 @@ func TestTargetedContainers(t *testing.T) {
 		"default": result,
 	}
 	c := &Config{RawGroups: rawGroups}
-	c.process()
-	containers = c.targetedContainers("")
+	c.expandEnv()
+	containers = c.explicitlyTargeted("")
 	if len(containers) != 2 || containers[0] != "a" || containers[1] != "b" {
 		t.Errorf("Expected %v, got %v", result, containers)
 	}
 	// If no default group, returns all containers
 	result = []string{"a", "b", "c"}
 	c = &Config{RawContainerMap: rawContainerMap}
-	c.process()
-	containers = c.targetedContainers("")
+	c.expandEnv()
+	containers = c.explicitlyTargeted("")
 	if len(containers) != 3 || containers[0] != "a" || containers[1] != "b" || containers[2] != "c" {
 		t.Errorf("Expected %v, got %v", result, containers)
 	}
@@ -77,14 +77,14 @@ func TestTargetedContainers(t *testing.T) {
 		"second": result,
 	}
 	c = &Config{RawContainerMap: rawContainerMap, RawGroups: rawGroups}
-	c.process()
-	containers = c.targetedContainers("second")
+	c.expandEnv()
+	containers = c.explicitlyTargeted("second")
 	if len(containers) != 2 || containers[0] != "b" || containers[1] != "c" {
 		t.Errorf("Expected %v, got %v", result, containers)
 	}
 	// Target is a container
 	result = []string{"a"}
-	containers = c.targetedContainers("a")
+	containers = c.explicitlyTargeted("a")
 	if len(containers) != 1 || containers[0] != "a" {
 		t.Errorf("Expected %v, got %v", result, containers)
 	}

crane/container_map.go
@@ -1,226 +1,0 @@
-package crane
-
-import (
-	"fmt"
-	"sort"
-	"strings"
-)
-
-// ContainerMap maps the container name
-// to its configuration
-type ContainerMap map[string]Container
-
-// DependencyGraph maps the container name
-// to its dependencies
-type DependencyGraph map[string]*Dependencies
-
-// Dependencies contain 3 fields:
-// list: contains all dependencies
-// linked: contains dependencies that
-// are being linked to.
-// net: container the net stack is shared with
-type Dependencies struct {
-	list   []string
-	linked []string
-	net    string
-}
-
-// includes checks whether the given needle is
-// included in the dependency list
-func (d *Dependencies) includes(needle string) bool {
-	for _, name := range d.list {
-		if name == needle {
-			return true
-		}
-	}
-	return false
-}
-
-// mustRun checks whether the given needle needs
-// to be running
-func (d *Dependencies) mustRun(needle string) bool {
-	if needle == d.net {
-		return true
-	}
-	for _, name := range d.linked {
-		if name == needle {
-			return true
-		}
-	}
-	return false
-}
-
-// satisfied is true when the list is empty
-func (d *Dependencies) satisfied() bool {
-	return len(d.list) == 0
-}
-
-// remove removes the given name from the list
-func (d *Dependencies) remove(resolved string) {
-	for i, name := range d.list {
-		if name == resolved {
-			d.list = append(d.list[:i], d.list[i+1:]...)
-		}
-	}
-}
-
-// returns a copy of the map where all containers are within the given list
-func (m ContainerMap) subset(included []string) ContainerMap {
-	newMap := make(ContainerMap)
-	for _, name := range included {
-		if container, present := m[name]; present {
-			newMap[name] = container
-		}
-	}
-	return newMap
-}
-
-// order works on the containerMap and retuens the order
-// The order can be reversed as well. This is needed as any command
-// bringing up containers needs to bring up the dependencies first,
-// but any command shutting down containers needs to shutdown the
-// dependent containers first.
-// If the order between 2 containers does not matter, they are sorted
-// alphabetically.
-// If the map cannot be resolved, and error is returned detailing
-// which containers still have unresolved dependencies.
-func (m ContainerMap) order(reversed bool) (order []string, err error) {
-	dependencyGraph := m.dependencies(reversed)
-	alphabetical := m.alphabetical(reversed)
-
-	success := true
-	for success && len(dependencyGraph) > 0 {
-		success = false
-		for _, name := range alphabetical {
-			if dependencies, ok := dependencyGraph[name]; ok {
-				if dependencies.satisfied() {
-					// Resolve "name" and continue with next iteration
-					success = true
-					order = append([]string{name}, order...)
-					dependencyGraph.resolve(name)
-					break
-				}
-			}
-		}
-
-		if !success && !reversed {
-			// Could not resolve a dependency so far in this iteration,
-			// but maybe one of the container already runs/exists?
-			// This check does only make sense for the default order.
-			for _, name := range alphabetical {
-				if dependencies, ok := dependencyGraph[name]; ok {
-					for _, name := range dependencies.list {
-						// Container must not be part of the map that
-						// is currently targeted.
-						if _, ok := m[name]; !ok {
-							// Need to "fake" a container here because
-							// it can't be retrieved from the map (as it was removed).
-							container := &Container{RawName: name}
-							satisfied := false
-							if dependencies.mustRun(name) {
-								satisfied = container.running()
-							} else {
-								satisfied = container.exists()
-							}
-							if satisfied {
-								success = true
-								dependencyGraph.resolve(name)
-								break
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	// If we still have dependencies, the container map
-	// cannot be resolved (cyclic or missing dependency found).
-	if len(dependencyGraph) > 0 {
-		unresolved := []string{}
-		for _, name := range alphabetical {
-			if _, ok := dependencyGraph[name]; ok {
-				unresolved = append(unresolved, name)
-			}
-		}
-		// For reversed order, that is okay.
-		// Otherwise, this is an error that needs
-		// to be resolved.
-		if reversed {
-			order = append(order, unresolved...)
-		} else {
-			err = fmt.Errorf("Dependencies for container(s) %s could not be resolved. Check for cyclic or missing dependencies.", strings.Join(unresolved, ", "))
-		}
-	}
-
-	return
-}
-
-// dependencies returns a map describing the dependencies
-// between the containers.
-func (m ContainerMap) dependencies(reversed bool) DependencyGraph {
-	dependencyGraph := make(DependencyGraph)
-
-	if reversed {
-		// Need to set every "dependency" as the key of the
-		// dependencyGraph map and list the containers that depend
-		// on it as the dependencies ...
-		// Iterate over map
-		for _, container := range m {
-			// Get dependency list of each container
-			for _, dep := range container.Dependencies().list {
-				// If dependencyGraph already has the key, append to the list,
-				// otherwise create that dependecy
-				if dependencies, ok := dependencyGraph[dep]; ok {
-					dependencies.list = append(dependencies.list, container.Name())
-				} else {
-					dependencyGraph[dep] = &Dependencies{list: []string{container.Name()}}
-				}
-			}
-			// If we haven't created the key yet, do it now
-			if _, ok := dependencyGraph[container.Name()]; !ok {
-				dependencyGraph[container.Name()] = &Dependencies{list: []string{}}
-			}
-		}
-	} else {
-		// For default order, just map the container names to their dependencies
-		for _, container := range m {
-			dependencyGraph[container.Name()] = container.Dependencies()
-		}
-	}
-
-	return dependencyGraph
-}
-
-// alphabetical returns the containers of the map in
-// alphabetical order. If reversed is set to true, it
-// returns reverse alphabetical order.
-func (m ContainerMap) alphabetical(reversed bool) []string {
-	alphabetical := []string{}
-
-	for name, _ := range m {
-		alphabetical = append(alphabetical, name)
-	}
-
-	sort.Strings(alphabetical)
-
-	if reversed {
-		reverse := []string{}
-		for _, name := range alphabetical {
-			reverse = append([]string{name}, reverse...)
-		}
-		alphabetical = reverse
-	}
-
-	return alphabetical
-}
-
-// resolve deletes the given name from the
-// dependencyGraph and removes it from all
-// dependencies.
-func (d DependencyGraph) resolve(resolved string) {
-	delete(d, resolved)
-	for _, dependencies := range d {
-		dependencies.remove(resolved)
-	}
-}

crane/container_map_test.go
@@ -1,99 +1,0 @@
-package crane
-
-import "testing"
-
-func TestSubset(t *testing.T) {
-	containerMap := ContainerMap{
-		"a": Container{},
-		"b": Container{},
-		"c": Container{},
-	}
-
-	subset := containerMap.subset([]string{"a", "c"})
-	if _, present := subset["a"]; !present {
-		t.Errorf("a should have been kept")
-	}
-	if _, present := subset["b"]; present {
-		t.Errorf("b should have been removed")
-	}
-	if _, present := subset["c"]; !present {
-		t.Errorf("c should have been kept")
-	}
-
-	subset = containerMap.subset([]string{"a", "a"})
-	if _, present := subset["a"]; !present {
-		t.Errorf("a should have been kept")
-	}
-	if _, present := subset["b"]; present {
-		t.Errorf("b should have been removed")
-	}
-	if _, present := subset["c"]; present {
-		t.Errorf("c should have been removed")
-	}
-
-	subset = containerMap.subset([]string{"d"})
-	if len(subset) != 0 {
-		t.Errorf("everything should have been removed but got %v", subset)
-	}
-}
-
-func TestOrder(t *testing.T) {
-	var err error
-	var order []string
-	var containerMap ContainerMap
-
-	// Resolvable map
-	containerMap = ContainerMap{
-		"b": Container{RawName: "b", Run: RunParameters{RawLink: []string{"c:c"}}},
-		"a": Container{RawName: "a", Run: RunParameters{RawLink: []string{"b:b"}}},
-		"c": Container{RawName: "c"},
-	}
-	// Default order
-	order, err = containerMap.order(false)
-	if err != nil || order[0] != "a" || order[1] != "b" || order[2] != "c" {
-		t.Errorf("Order should have been [a b c], got %v. Err: %v", order, err)
-	}
-	// Reversed order
-	order, err = containerMap.order(true)
-	if err != nil || order[0] != "c" || order[1] != "b" || order[2] != "a" {
-		t.Errorf("Order should have been [c b a], got %v. Err: %v", order, err)
-	}
-
-	// Unresolvable map
-	containerMap = ContainerMap{
-		"b": Container{RawName: "b", Run: RunParameters{RawLink: []string{"c:c"}}},
-		"a": Container{RawName: "a", Run: RunParameters{RawLink: []string{"b:b"}}},
-		"c": Container{RawName: "c", Run: RunParameters{RawLink: []string{"a:a"}}},
-	}
-	// Errors in default order
-	order, err = containerMap.order(false)
-	if err == nil {
-		t.Errorf("Cyclic dependency a -> b -> c -> a should not have been resolvable, got %v. Err: %v", order, err)
-	}
-	// Works in reversed order
-	order, err = containerMap.order(true)
-	if err != nil || order[0] != "c" || order[1] != "b" || order[2] != "a" {
-		t.Errorf("Order should have been [c b a], got %v", order)
-	}
-}
-
-func TestAlphabetical(t *testing.T) {
-	var containerMap ContainerMap
-
-	containerMap = ContainerMap{
-		"b": Container{},
-		"c": Container{},
-		"a": Container{},
-		"e": Container{},
-		"d": Container{},
-	}
-
-	alphabetical := containerMap.alphabetical(false)
-	if alphabetical[0] != "a" || alphabetical[1] != "b" || alphabetical[2] != "c" || alphabetical[3] != "d" || alphabetical[4] != "e" {
-		t.Errorf("Order should have been [a b c d e], got %v", alphabetical)
-	}
-	alphabetical = containerMap.alphabetical(true)
-	if alphabetical[0] != "e" || alphabetical[1] != "d" || alphabetical[2] != "c" || alphabetical[3] != "b" || alphabetical[4] != "a" {
-		t.Errorf("Order should have been [e d c b a], got %v", alphabetical)
-	}
-}

crane/containers.go
@@ -9,6 +9,14 @@ import (
 
 type Containers []Container
 
+func (containers Containers) reversed() Containers {
+	var reversed []Container
+	for i := len(containers) - 1; i >= 0; i-- {
+		reversed = append(reversed, containers[i])
+	}
+	return reversed
+}
+
 // Lift containers (provision + run).
 // When recreate is set, this will re-provision all images
 // and recreate all containers.

commit fc0cf6f0b42c3aecdfd5222b2dc95951e8a106a1

fix regressions introduced in 71b9c0a

crane/dependency_graph.go
@@ -22,13 +22,14 @@ func (graph DependencyGraph) order(target Target, force bool) (order []string, e
 
 		// Try to resolve container with satisfied dependencies
 		for _, name := range target {
-			dependencies := graph[name]
-			if dependencies.satisfied() {
-				// Resolve "name" and continue with next iteration
-				success = true
-				order = append([]string{name}, order...)
-				graph.resolve(name)
-				break
+			if dependencies, ok := graph[name]; ok {
+				if dependencies.satisfied() {
+					// Resolve "name" and continue with next iteration
+					success = true
+					order = append([]string{name}, order...)
+					graph.resolve(name)
+					break
+				}
 			}
 		}
 
@@ -36,20 +37,21 @@ func (graph DependencyGraph) order(target Target, force bool) (order []string, e
 		// we check if one of the non-targeted dependencies already runs or exists.
 		if !success {
 			for _, name := range target {
-				dependencies := graph[name]
-				for _, name := range dependencies.all {
-					if !target.includes(name) {
-						container := &Container{RawName: name}
-						satisfied := false
-						if dependencies.mustRun(name) {
-							satisfied = container.running()
-						} else {
-							satisfied = container.exists()
-						}
-						if satisfied {
-							success = true
-							graph.resolve(name)
-							break
+				if dependencies, ok := graph[name]; ok {
+					for _, name := range dependencies.all {
+						if !target.includes(name) {
+							container := &Container{RawName: name}
+							satisfied := false
+							if dependencies.mustRun(name) {
+								satisfied = container.running()
+							} else {
+								satisfied = container.exists()
+							}
+							if satisfied {
+								success = true
+								graph.resolve(name)
+								break
+							}
 						}
 					}
 				}
@@ -62,12 +64,13 @@ func (graph DependencyGraph) order(target Target, force bool) (order []string, e
 		// dependency.
 		if !success && force {
 			for _, name := range target {
-				dependencies := graph[name]
-				for _, name := range dependencies.all {
-					if !target.includes(name) {
-						success = true
-						graph.resolve(name)
-						break
+				if dependencies, ok := graph[name]; ok {
+					for _, name := range dependencies.all {
+						if !target.includes(name) {
+							success = true
+							graph.resolve(name)
+							break
+						}
 					}
 				}
 			}

crane/dependency_graph_test.go
@@ -13,7 +13,7 @@ func TestOrder(t *testing.T) {
 		"a": &Dependencies{all: []string{"b"}},
 		"c": &Dependencies{all: []string{}},
 	}
-	order, err = dependencyGraph.order([]string{"a", "b", "c"}, false)
+	order, err = dependencyGraph.order([]string{"b", "c", "a"}, false)
 	if err != nil || len(order) != 3 || order[0] != "a" || order[1] != "b" || order[2] != "c" {
 		t.Errorf("Order should have been [a b c], got %v. Err: %v", order, err)
 	}