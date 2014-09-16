1e20ed7de934a3f83a37ba8fe53633a4304867d6
Brice Jaglin <bjaglin@teads.tv>
2014-09-16 09:45:32 +0200

    introduce minimal/recommended docker client version check

--- crane/crane.go
+++ crane/crane.go
@@ -6,6 +6,7 @@ import (
 	"github.com/michaelsauter/crane/print"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"syscall"
 )
@@ -14,6 +15,11 @@ type StatusError struct {
 	error  error
 	status int
 }
+
+var (
+	minimalDockerVersion     = []int{1, 0}
+	recommendedDockerVersion = []int{1, 2}
+)
 
 func RealMain() {
 	// On panic, recover the error, display it and return the given status code if any
@@ -36,8 +42,55 @@ func RealMain() {
 		}
 		os.Exit(statusError.status)
 	}()
-
+	checkDockerClient()
 	handleCmd()
+}
+
+// Ensure there is a docker binary in the path, panicking if its version
+// is below the minimal requirement, and printing a warning if its version
+// is below the recommended requirement.
+func checkDockerClient() {
+	dockerCmd := []string{"docker", "--version"}
+	sedCmd := []string{"sed", "-e", "s/[^0-9]*\\([0-9.]\\+\\).*/\\1/"}
+	output, err := pipedCommandOutput(dockerCmd, sedCmd)
+	if err != nil {
+		panic(StatusError{errors.New("Error when probing Docker's client version. Is docker installed and within the $PATH?"), 69})
+	}
+	rawVersions := strings.Split(string(output), ".")[:2]
+	var versions []int
+	for _, rawVersion := range rawVersions {
+		version, err := strconv.Atoi(rawVersion)
+		if err != nil {
+			panic(StatusError{fmt.Errorf("Error when parsing Docker's version %v: %v", rawVersion, err), 69})
+		}
+		versions = append(versions, version)
+	}
+	for i, expectedVersion := range minimalDockerVersion {
+		if versions[i] > expectedVersion {
+			break
+		}
+		if versions[i] < expectedVersion {
+			panic(StatusError{fmt.Errorf("Unsupported client version. Please upgrade to Docker %v or later.", intJoin(recommendedDockerVersion, ".")), 69})
+		}
+	}
+	for i, expectedVersion := range recommendedDockerVersion {
+		if versions[i] > expectedVersion {
+			break
+		}
+		if versions[i] < expectedVersion {
+			print.Noticef("WARNING: outdated Docker client, behavior might not be optimal. Please upgrade to Docker %v or later.\n", intJoin(recommendedDockerVersion, "."))
+			break
+		}
+	}
+}
+
+// Similar to strings.Join() for int slices.
+func intJoin(intSlice []int, sep string) string {
+	var stringSlice []string
+	for _, v := range intSlice {
+		stringSlice = append(stringSlice, fmt.Sprint(v))
+	}
+	return strings.Join(stringSlice, ".")
 }
 
 func executeCommand(name string, args []string) {
