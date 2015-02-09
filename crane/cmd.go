45565afbd1970e00048e5fa7f5e3bc68eb8e3ae3
Michael Sauter <mail@michaelsauter.net>
2015-02-09 02:33:24 -0800

    Bump version to 1.1.0

--- crane/cmd.go
+++ crane/cmd.go
@@ -210,7 +210,7 @@ net=container relations.`,
 		Short: "Display version",
 		Long:  `Displays the version of Crane.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("v1.0.0")
+			fmt.Println("v1.1.0")
 		},
 	}
 
