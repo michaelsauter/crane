82d830ecf13cb311ae59dd2b555100803b556f20
Michael Sauter <mail@michaelsauter.net>
2015-05-29 11:56:19 -0700

    Bump version to 1.3.1

--- crane/cmd.go
+++ crane/cmd.go
@@ -221,7 +221,7 @@ net=container relations.`,
 		Short: "Display version",
 		Long:  `Displays the version of Crane.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("v1.3.0")
+			fmt.Println("v1.3.1")
 		},
 	}
 
--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 # Set version to latest unless set by user
 if [ -z "$VERSION" ]; then
-  VERSION="1.3.0"
+  VERSION="1.3.1"
 fi
 
 echo "Downloading version ${VERSION}..."
