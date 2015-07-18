3aea1bc0816cd3cffe5c68efaaec08f49605532d
Michael Sauter <mail@michaelsauter.net>
2015-07-18 05:39:51 -0700

    Fix a few test errors

--- crane/containers_test.go
+++ crane/containers_test.go
@@ -5,17 +5,10 @@ import (
 	"testing"
 )
 
-func TestNames(t *testing.T) {
-	var containers Containers
-	containers = []Container{&container{RawName: "a"}, &container{RawName: "b"}}
-	assert.Equal(t, []string{"a", "b"}, containers.names())
-
-}
-
 func TestReversed(t *testing.T) {
 	var containers Containers
 	containers = []Container{&container{RawName: "a"}, &container{RawName: "b"}}
-	reversed := containers.reversed()
+	reversed := containers.Reversed()
 	assert.Len(t, reversed, 2)
 	assert.Equal(t, "b", reversed[0].Name())
 	assert.Equal(t, "a", reversed[1].Name())
