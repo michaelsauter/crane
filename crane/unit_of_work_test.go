5493b44920f4bcdd150ea5b9e7f120b904cd33c1
Michael Sauter <mail@michaelsauter.net>
2015-07-21 11:43:16 -0700

    Add tests for unit of work creation

---
+++ crane/unit_of_work_test.go
@@ -0,0 +1,67 @@
+package crane
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewUnitOfWork(t *testing.T) {
+
+	examples := []struct {
+		graph    DependencyGraph
+		targeted []string
+		expected *UnitOfWork
+		err      bool
+	}{
+		{ // resolvable map -> works
+			graph: DependencyGraph{
+				"b": &Dependencies{All: []string{"c"}},
+				"a": &Dependencies{All: []string{"b"}},
+				"c": &Dependencies{All: []string{}},
+			},
+			targeted: []string{"a", "b", "c"},
+			expected: &UnitOfWork{
+				targeted:   []string{"a", "b", "c"},
+				containers: []string{"a", "b", "c"},
+				order:      []string{"c", "b", "a"},
+				mustRun:    []string{},
+			},
+			err: false,
+		},
+		{ // cyclic map -> fails
+			graph: DependencyGraph{
+				"b": &Dependencies{All: []string{"c"}},
+				"a": &Dependencies{All: []string{"b"}},
+				"c": &Dependencies{All: []string{"a"}},
+			},
+			targeted: []string{"a", "b", "c"},
+			err:      true,
+		},
+		{ // partial target -> works
+			graph: DependencyGraph{
+				"b": &Dependencies{All: []string{"c"}, Link: []string{"c"}},
+				"a": &Dependencies{All: []string{"b"}},
+				"c": &Dependencies{All: []string{}},
+			},
+			targeted: []string{"a", "b"},
+			expected: &UnitOfWork{
+				targeted:   []string{"a", "b"},
+				containers: []string{"a", "b", "c"},
+				order:      []string{"c", "b", "a"},
+				mustRun:    []string{"c"},
+			},
+			err: false,
+		},
+	}
+
+	for _, example := range examples {
+		uow, err := NewUnitOfWork(example.graph, example.targeted)
+		if example.err {
+			assert.Error(t, err)
+		} else {
+			if assert.NoError(t, err) {
+				assert.Equal(t, example.expected, uow)
+			}
+		}
+	}
+}
