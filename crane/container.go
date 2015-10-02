da7413dbb4bc4af4dc8df68ea5ec41103ecd021a
Dreamcat4 <dreamcat4@gmail.com>
2015-10-02 12:01:24 +0100

    Fix platform-independant path separator

--- crane/container.go
+++ crane/container.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -998,7 +999,7 @@ func (c *container) buildImage(nocache bool) {
 	}
 	args = append(args, "--rm", "--tag="+c.Image())
 	if len(c.BuildFile()) > 0 {
-		args = append(args, "--file="+c.BuildContext()+os.PathSeparator+c.BuildFile())
+		args = append(args, "--file="+filepath.FromSlash(c.BuildContext()+"/"+c.BuildFile()))
 	}
 	args = append(args, c.BuildContext())
 	executeCommand("docker", args)
