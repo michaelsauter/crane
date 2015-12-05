michaelsauter/crane

commit 8f64dd4a03645aa6efad7201e520c261c1839edb

Fix problems when dependencies are marked as unique

crane/container.go
@@ -10,7 +10,6 @@ import (
 	"path"
 	"strconv"
 	"strings"
-	"time"
 )
 
 type Container interface {
@@ -22,6 +21,7 @@ type Container interface {
 	Dependencies(excluded []string) *Dependencies
 	Exists() bool
 	Running() bool
+	Unique() bool
 	Paused() bool
 	ImageExists() bool
 	Status() []string
@@ -44,7 +44,7 @@ type Container interface {
 type container struct {
 	id            string
 	RawName       string
-	Unique        bool            `json:"unique" yaml:"unique"`
+	RawUnique     bool            `json:"unique" yaml:"unique"`
 	RawDockerfile string          `json:"dockerfile" yaml:"dockerfile"`
 	RawImage      string          `json:"image" yaml:"image"`
 	RunParams     RunParameters   `json:"run" yaml:"run"`
@@ -186,6 +186,10 @@ func (c *container) Dockerfile() string {
 
 func (c *container) Image() string {
 	return os.ExpandEnv(c.RawImage)
+}
+
+func (c *container) Unique() bool {
+	return c.RawUnique
 }
 
 func (c *container) ImageWithTag() string {
@@ -455,7 +459,7 @@ func (r *RunParameters) Cmd() []string {
 }
 
 func (c *container) Id() string {
-	if len(c.id) == 0 && !c.Unique {
+	if len(c.id) == 0 && !c.Unique() {
 		// `docker inspect` works for both image and containers, make sure this is a
 		// container payload we get back, otherwise we might end up getting the Id
 		// of the image of the same name.
@@ -488,7 +492,7 @@ func (c *container) ImageExists() bool {
 }
 
 func (c *container) Status() []string {
-	if c.Unique {
+	if c.Unique() {
 		fmt.Printf("Cannot show status of uniquely named container(s) %s.\n", c.Name())
 		return []string{c.Name(), c.Image(), "-", "-", "-", "-", "-"}
 	}
@@ -517,8 +521,8 @@ func (c *container) Provision(nocache bool) {
 
 // Create container
 func (c *container) Create(cmds []string, excluded []string, configPath string) {
-	if c.Unique {
-		c.setUniqueName()
+	if c.Unique() {
+		c.RawName = nameWithUniqueId(c.RawName)
 	} else {
 		c.Rm(true)
 	}
@@ -530,8 +534,8 @@ func (c *container) Create(cmds []string, excluded []string, configPath string) 
 
 // Run container, or start it if already existing
 func (c *container) Run(cmds []string, excluded []string, configPath string) {
-	if c.Unique {
-		c.setUniqueName()
+	if c.Unique() {
+		c.RawName = nameWithUniqueId(c.RawName)
 	} else {
 		c.Rm(true)
 	}
@@ -650,8 +654,13 @@ func (c *container) createArgs(cmds []string, excluded []string, configPath stri
 	}
 	// Link
 	for _, link := range c.RunParams.Link() {
-		if !includes(excluded, strings.Split(link, ":")[0]) {
-			args = append(args, "--link", link)
+		linkParts := strings.Split(link, ":")
+		linkName := linkParts[0]
+		if !includes(excluded, linkName) {
+			if cfg.Container(linkName).Unique() {
+				linkParts[0] = nameWithUniqueId(linkName)
+			}
+			args = append(args, "--link", strings.Join(linkParts, ":"))
 		}
 	}
 	// LogDriver
@@ -681,7 +690,11 @@ func (c *container) createArgs(cmds []string, excluded []string, configPath stri
 	// Net
 	if c.RunParams.Net() != "bridge" {
 		if !includes(excluded, c.netContainer()) {
-			args = append(args, "--net", c.RunParams.Net())
+			netName := c.RunParams.Net()
+			if cfg.Container(netName).Unique() {
+				netName = nameWithUniqueId(netName)
+			}
+			args = append(args, "--net", netName)
 		}
 	}
 	// OomKillDisable
@@ -746,8 +759,13 @@ func (c *container) createArgs(cmds []string, excluded []string, configPath stri
 	}
 	// VolumesFrom
 	for _, volumesFrom := range c.RunParams.VolumesFrom() {
-		if !includes(excluded, strings.Split(volumesFrom, ":")[0]) {
-			args = append(args, "--volumes-from", volumesFrom)
+		volumesFromParts := strings.Split(volumesFrom, ":")
+		volumesFromName := volumesFromParts[0]
+		if !includes(excluded, volumesFromName) {
+			if cfg.Container(volumesFromName).Unique() {
+				volumesFromParts[0] = nameWithUniqueId(volumesFromName)
+			}
+			args = append(args, "--volumes-from", strings.Join(volumesFromParts, ":"))
 		}
 	}
 	// Workdir
@@ -769,7 +787,7 @@ func (c *container) createArgs(cmds []string, excluded []string, configPath stri
 
 // Start container
 func (c *container) Start(excluded []string, configPath string) {
-	if c.Unique {
+	if c.Unique() {
 		fmt.Printf("Cannot start uniquely named container(s) %s.\n", c.Name())
 		return
 	}
@@ -794,7 +812,7 @@ func (c *container) Start(excluded []string, configPath string) {
 
 // Kill container
 func (c *container) Kill() {
-	if c.Unique {
+	if c.Unique() {
 		fmt.Printf("Cannot kill uniquely named container(s) %s.\n", c.Name())
 		return
 	}
@@ -809,7 +827,7 @@ func (c *container) Kill() {
 
 // Stop container
 func (c *container) Stop() {
-	if c.Unique {
+	if c.Unique() {
 		fmt.Printf("Cannot stop uniquely named container(s) %s.\n", c.Name())
 		return
 	}
@@ -824,7 +842,7 @@ func (c *container) Stop() {
 
 // Pause container
 func (c *container) Pause() {
-	if c.Unique {
+	if c.Unique() {
 		fmt.Printf("Cannot pause uniquely named container(s) %s.\n", c.Name())
 		return
 	}
@@ -843,7 +861,7 @@ func (c *container) Pause() {
 
 // Unpause container
 func (c *container) Unpause() {
-	if c.Unique {
+	if c.Unique() {
 		fmt.Printf("Cannot unpause uniquely named container(s) %s.\n", c.Name())
 		return
 	}
@@ -856,7 +874,7 @@ func (c *container) Unpause() {
 
 // Remove container
 func (c *container) Rm(force bool) {
-	if c.Unique {
+	if c.Unique() {
 		fmt.Printf("Cannot remove uniquely named container(s) %s.\n", c.Name())
 		return
 	}
@@ -888,7 +906,7 @@ func (c *container) Rm(force bool) {
 
 // Dump container logs
 func (c *container) Logs(follow bool, since string, tail string) (stdout, stderr io.Reader) {
-	if c.Unique {
+	if c.Unique() {
 		fmt.Printf("Cannot show logs of uniquely named container(s) %s.\n", c.Name())
 		return
 	}
@@ -947,9 +965,8 @@ func (c *container) buildImage(nocache bool) {
 	executeCommand("docker", args)
 }
 
-func (c *container) setUniqueName() {
-	milliseconds := time.Now().UnixNano() / 1000000
-	c.RawName = c.RawName + "-" + strconv.FormatInt(milliseconds, 10)
+func nameWithUniqueId(name string) string {
+	return name + "-" + cfg.UniqueId()
 }
 
 // Return the image id of a tag, or an empty string if it doesn't exist

crane/config.go
@@ -10,13 +10,16 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type Config interface {
 	DependencyGraph(excluded []string) DependencyGraph
 	ContainersForReference(reference string) (result []string)
 	Path() string
+	UniqueId() string
 	ContainerMap() ContainerMap
 	Container(name string) Container
 }
@@ -28,6 +31,7 @@ type config struct {
 	containerMap    ContainerMap
 	groups          map[string][]string
 	path            string
+	uniqueId        string
 }
 
 // ContainerMap maps the container name
@@ -146,12 +150,18 @@ func NewConfig(location string) Config {
 	config = readConfig(configFile)
 	config.initialize()
 	config.path = path.Dir(configFile)
+	milliseconds := time.Now().UnixNano() / 1000000
+	config.uniqueId = strconv.FormatInt(milliseconds, 10)
 	return config
 }
 
 // Return path of config file
 func (c *config) Path() string {
 	return c.path
+}
+
+func (c *config) UniqueId() string {
+	return c.uniqueId
 }
 
 func (c *config) ContainerMap() ContainerMap {

commit fdbcd614085fde1bd01a6577b145edfe5283f709

old flag missing: `docker run --ipc=container:foo`
also fixes the rewrite of a `--net=container:foo` when foo is a
container (was `--net=foo`), regression since 8f64dd4

crane/container.go
@@ -91,6 +91,7 @@ type RunParameters struct {
 	RawGroupAdd          []string    `json:"group-add" yaml:"group-add"`
 	RawHostname          string      `json:"hostname" yaml:"hostname"`
 	Interactive          bool        `json:"interactive" yaml:"interactive"`
+	RawIPC               string      `json:"ipc" yaml:"ipc"`
 	RawKernelMemory      string      `json:"kernel-memory" yaml:"kernel-memory"`
 	RawLabel             interface{} `json:"label" yaml:"label"`
 	RawLabelFile         []string    `json:"label-file" yaml:"label-file"`
@@ -174,17 +175,6 @@ func (o OptBool) Falsy() bool {
 	return o.Defined && !o.Value
 }
 
-func (c *container) netContainer() (name string) {
-	if netParts := strings.Split(c.RunParams().Net(), ":"); len(netParts) == 2 && netParts[0] == "container" {
-		// We'll just assume here that the reference is a name, and not an id, even
-		// though docker supports it, since we have no bullet-proof way to tell:
-		// heuristics to detect whether it's an id could bring false positives, and
-		// a lookup in the list of container names could bring false negatives
-		name = netParts[1]
-	}
-	return
-}
-
 func (c *container) BuildParams() BuildParameters {
 	return c.RawBuild
 }
@@ -221,9 +211,14 @@ func (c *container) Dependencies() *Dependencies {
 			dependencies.VolumesFrom = append(dependencies.VolumesFrom, volumesFromName)
 		}
 	}
-	if dependencies.Net = c.netContainer(); dependencies.Net != "" {
+	if dependencies.Net = containerReference(c.RunParams().Net()); dependencies.Net != "" {
 		if !includes(excluded, dependencies.Net) && !dependencies.includes(dependencies.Net) {
 			dependencies.All = append(dependencies.All, dependencies.Net)
+		}
+	}
+	if dependencies.IPC = containerReference(c.RunParams().IPC()); dependencies.IPC != "" {
+		if !includes(excluded, dependencies.IPC) && !dependencies.includes(dependencies.IPC) {
+			dependencies.All = append(dependencies.All, dependencies.IPC)
 		}
 	}
 	return dependencies
@@ -381,6 +376,10 @@ func (r RunParameters) GroupAdd() []string {
 
 func (r RunParameters) Hostname() string {
 	return os.ExpandEnv(r.RawHostname)
+}
+
+func (r RunParameters) IPC() string {
+	return os.ExpandEnv(r.RawIPC)
 }
 
 func (r RunParameters) KernelMemory() string {
@@ -746,6 +745,17 @@ func (c *container) createArgs(cmds []string, excluded []string) []string {
 	if c.RunParams().Interactive {
 		args = append(args, "--interactive")
 	}
+	// IPC
+	if len(c.RunParams().IPC()) > 0 {
+		ipcContainer := containerReference(c.RunParams().IPC())
+		if len(ipcContainer) > 0 {
+			if !includes(excluded, ipcContainer) {
+				args = append(args, "--ipc", "container:"+cfg.Container(ipcContainer).ActualName())
+			}
+		} else {
+			args = append(args, "--ipc", c.RunParams().IPC())
+		}
+	}
 	// KernelMemory
 	if len(c.RunParams().KernelMemory()) > 0 {
 		args = append(args, "--kernel-memory", c.RunParams().KernelMemory())
@@ -801,9 +811,10 @@ func (c *container) createArgs(cmds []string, excluded []string) []string {
 	}
 	// Net
 	if c.RunParams().Net() != "bridge" {
-		if len(c.netContainer()) > 0 {
-			if !includes(excluded, c.netContainer()) {
-				args = append(args, "--net", cfg.Container(c.netContainer()).ActualName())
+		netContainer := containerReference(c.RunParams().Net())
+		if len(netContainer) > 0 {
+			if !includes(excluded, netContainer) {
+				args = append(args, "--net", "container:"+cfg.Container(netContainer).ActualName())
 			}
 		} else {
 			args = append(args, "--net", c.RunParams().Net())
@@ -1132,6 +1143,18 @@ func imageIDFromTag(tag string) string {
 		return ""
 	}
 	return string(output)
+}
+
+// If the reference follows the `container:foo` pattern, return "foo"; otherwise, return an empty string
+func containerReference(reference string) (name string) {
+	if parts := strings.Split(reference, ":"); len(parts) == 2 && parts[0] == "container" {
+		// We'll just assume here that the reference is a name, and not an id, even
+		// though docker supports it, since we have no bullet-proof way to tell:
+		// heuristics to detect whether it's an id could bring false positives, and
+		// a lookup in the list of container names could bring false negatives
+		name = parts[1]
+	}
+	return
 }
 
 // Attempt to parse the value referenced by the go template

crane/dependencies.go
@@ -10,6 +10,7 @@ type Dependencies struct {
 	Link        []string
 	VolumesFrom []string
 	Net         string
+	IPC         string
 }
 
 // includes checks whether the given needle is
@@ -26,7 +27,7 @@ func (d *Dependencies) includes(needle string) bool {
 // requireStarted checks whether the given needle needs
 // to be running in order to be satisfied.
 func (d *Dependencies) requireStarted(needle string) bool {
-	if needle == d.Net {
+	if needle == d.Net || needle == d.IPC {
 		return true
 	}
 	for _, name := range d.Link {

crane/dependencies_test.go
@@ -7,14 +7,16 @@ import (
 
 func TestIncludes(t *testing.T) {
 	dependencies := Dependencies{
-		All:         []string{"link", "volumesFrom", "net"},
+		All:         []string{"link", "volumesFrom", "net", "ipc"},
 		Link:        []string{"link"},
 		VolumesFrom: []string{"volumesFrom"},
 		Net:         "net",
+		IPC:         "ipc",
 	}
 	assert.True(t, dependencies.includes("link"))
 	assert.True(t, dependencies.includes("volumesFrom"))
 	assert.True(t, dependencies.includes("net"))
+	assert.True(t, dependencies.includes("ipc"))
 	assert.False(t, dependencies.includes("non-existent"))
 }
 